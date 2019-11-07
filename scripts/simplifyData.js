2ca04d7945c85964d075192fd68492de874e0447
Courtney Simonse <courtney.r.elder@gmail.com>
2019-11-06 22:38:06 -0500

    keep OBJECTID in data files

--- scripts/simplifyData.js
+++ scripts/simplifyData.js
@@ -12,7 +12,7 @@ function simplifyFiles() {
     if (err) throw err;
 
     // filter properties to only keep "PARKDISTRICT", simplify verticies and coordinate precision, output to geojson
-    const commands = '-filter-fields PARKDISTRICT -simplify dp 5% -o data/boundaries.json precision=.0001 format=geojson';
+    const commands = '-filter-fields OBJECTID,PARKDISTRICT -simplify dp 5% -o data/boundaries.json precision=.0001 format=geojson';
 
     // run mapshaper on the park district data with the commands above
     mapshaper.runCommands(commands, geojson, (err) => {
@@ -29,7 +29,7 @@ function simplifyFiles() {
         if (err) throw err;
 
         // filter properties to only keep "PARKDISTRICT", simplify verticies and coordinate precision, output to geojson
-        const commands = '-filter-fields TRAILNAME,PARKDISTRICT -simplify dp 15% -o data/trails.json precision=.0001 format=geojson';
+        const commands = '-filter-fields OBJECTID,TRAILNAME,PARKDISTRICT -simplify dp 15% -o data/trails.json precision=.0001 format=geojson';
 
         // run mapshaper on the park district data with the commands above
         mapshaper.runCommands(commands, geojson, (err) => {
