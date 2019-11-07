b89e7b8c794b85af49d1b7cbf935afd037e6cd89
Courtney Simonse <courtney.r.elder@gmail.com>
2019-11-06 22:05:21 -0500

    add mapshaper commands to filter fields on trail signs and reduce coordinate precision

--- scripts/simplifyData.js
+++ scripts/simplifyData.js
@@ -40,6 +40,26 @@ function simplifyFiles() {
           console.log(chalk.green('trails simplified and saved!'));
 
         });
+
+        // read districts boundaries asynchronously
+        fs.readFile(__dirname + "/../project-files/trail-signs.geojson", 'utf8', (err, geojson) => {
+
+          // output error to console
+          if (err) throw err;
+
+          // filter properties to only keep "PARKDISTRICT", simplify verticies and coordinate precision, output to geojson
+          const commands = '-filter-fields OBJECTID,PARKDISTRICT,SIGN_TYPE -o data/signs.json precision=.0001 format=geojson';
+
+          // run mapshaper on the park district data with the commands above
+          mapshaper.runCommands(commands, geojson, (err) => {
+
+            // output error to console
+            if (err) throw err;
+
+            console.log(chalk.green('trail signs simplified and saved!'));
+
+          });
+        });
       });
     });
 
